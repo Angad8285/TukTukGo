815d70aa44bf59d583c46fca71af94b9ea3bc68b
Angad Singh <angad.smart005@gmail.com>
2023-02-27 00:04:25 +0530

    start encryption

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { stringify } = require('querystring');
-// const { appendFile } = require('fs');
+// var engines = require('consolidate');
+const { appendFile } = require('fs');
+const { BlockList } = require('net');
 
 const app = express();
 const port = 6969;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+// app.set('views', __dirname + '/views');
+// app.engine('html', engines.mustache);
+// app.set('view engine', 'html');
 app.use(express.static('public'));
+
+///////////////////////////////////////////// . GLOBAL VARIABLES      ///////////////////////
+
+var isLoggedIn = false;
+const spots = ["Main Gate","Auditorium", "Jaggi Complex", "G-Block", "Library", "Tan Building", "Dispencary", "H-Hostel", "J-Hostel", "Cos Complex", "M-Hostel", "L-Hostel", "K-Hostel", "A-Hostel", "B-Hostel", "C-Hostel", "I-Hostel", "PG-Hostel", "Q-Hostel", "E-block"];
+
 
 //////////////////////////////////////////// .  MONGODB CONNECTION    ///////////////////////
 mongoose.set('strictQuery', true);
@@ -26,7 +39,7 @@ const Schema = mongoose.Schema
 const driverSchema = new Schema({
     diLocation: String,
     dfLocation: String,
-    dNumber: Number,
+    dNumber: String,
     occupancy: Number
 });
 
@@ -48,10 +61,117 @@ const driver = mongoose.model('driver', driverSchema);
 const student = mongoose.model('student', studentSchema);
 const stuList = mongoose.model('stuList', stuListSchema);
 /////////////////////////////////////////// .   MODEL FORMATIONS DONE    ////////////////////////////////
-app.get("/", (req, res) => {
-    res.render(__dirname + "/public/index.ejs");
+
+
+////////////////////////////////////////// .   DUMMY DATA /////////////////////
+ 
+/////////////// . DUMMUY DATA FOR DRIVERS   //////////////////
+function dummyDrivers() {
+    for (var i = 0; i < 15; i++ ) {
+        var random1 = Math.floor(Math.random() * (spots.length) );
+        var random2 = Math.floor(Math.random() * (spots.length) );
+        var random3 = Math.floor(Math.random() * (5) );
+        if ( random1 !== random2) {
+            const autoDriver = new driver ({
+                diLocation: spots[random1],
+                dfLocation: spots[random2],
+                dNumber: "+91 94534 34232",
+                occupancy: random3
+            })
+
+            autoDriver.save((err, res) => {
+                if (err) {
+                    console.log("error: " + err)
+                } else {
+                    console.log("response: " + res)
+                }
+            })
+        }
+    }
+}
+
+// function deleteDummy() {
+//     driver.deleteMany({dNumber: "+91 94534 34232"}, (err, db) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             db.close;
+//         }
+//     })
+// }
+
+
+// app.get("/n", (req, res) => {
+//     
+//     console.log(spots.length);
+//     console.log(random1);
+// })
+
+app.get("/signin", (req, res) => {
+    // res.render(__dirname + "/public/index.ejs");
+    // res.render(__dirname + '/public/signup-signin/signin.html')
+    // dummy();
+    res.render(__dirname + "/public/signin.html");
+})
+
+app.post('/signin', (req, result) => {
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    // console.log(req.body.name);
+    const newStudent = new stuList({
+        name: name,
+        username: username,
+        password: password
+    })
+
+    var unAlreadyExists = 0;
+
+    stuList.find({username: req.body.username}, (err, res) => {
+        if (err) {
+            console.log(err)
+            console.log("err")
+        } else {
+            if (res.length !== 0 ) {
+                var passwordMatches = false
+                res.forEach(stu => {
+                    if (stu.password == password) {
+                        passwordMatches = true;
+                    }
+                });
+
+                if (passwordMatches) {
+                    result.send("You are now logged in.");
+                    isLoggedIn = true;
+                } else {
+                    result.send("The username you entered is already in use, use of some other username. IT SHOULD BE UNIQUE.")
+                }
+
+            } else {
+                newStudent.save((err, res) => {
+                    if (err) return handleError(err);
+                    else return console.log("saved successfully")
+                });
+
+                isLoggedIn = true;
+                
+                result.redirect("/home")
+            }
+        }
+    })
+
+    // console.log(newStudent.name);
+    
+})
+
+app.get("/home", (req, res) => {
+    if (isLoggedIn) {
+        res.render(__dirname + "/public/home.html")
+    } else {
+        res.redirect("/signin");
+    }
 })
 
 app.listen(port, () => {
-    console.log("Hello world");
+    console.log(port);
 })
